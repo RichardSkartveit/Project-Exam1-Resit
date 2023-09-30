2093817270c33d510247aa535f07428f8c3cc99b
RichardSkartveit <114581981+RichardSkartveit@users.noreply.github.com>
2023-10-01 01:20:09 +0200

    trying to get it work..

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const apiUrl = 'http://localhost/wordpress/wp-json/wp/v2/posts';
+const apiUrl = 'http://goodfoodmood0.wordpress.com/wp-json/wp/v2/posts';
+console.log('JavaScript code is running.');
 
 fetch(apiUrl)
     .then(response => response.json())
     .then(posts => {
         const postList = document.getElementById('post-list');
 
-        
         posts.forEach(post => {
             const postElement = document.createElement('div');
             postElement.classList.add('post');
@@ -19,3 +19,26 @@ fetch(apiUrl)
     .catch(error => {
         console.error('Error fetching posts:', error);
     });
+
+
+    
+const recipeSearchInput = document.getElementById('recipeSearch');
+const recipeSections = document.querySelectorAll('.post');
+
+
+recipeSearchInput.addEventListener('input', () => {
+  const searchTerm = recipeSearchInput.value.toLowerCase();
+
+  
+  recipeSections.forEach(section => {
+    const recipeTitle = section.querySelector('h2').textContent.toLowerCase();
+    if (recipeTitle.includes(searchTerm)) {
+      section.style.display = 'block';
+    } else {
+      section.style.display = 'none';
+    }
+  });
+});
+
+
+
