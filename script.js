ebc13b485e5dcea739e57bd8bda5e13bf7453707
RichardSkartveit <114581981+RichardSkartveit@users.noreply.github.com>
2023-10-01 22:12:34 +0200

    some issues i tried to fix

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
-const apiUrl = 'http://goodfoodmood0.wordpress.com/wp-json/wp/v2/posts';
-console.log('JavaScript code is running.');
-
-fetch(apiUrl)
-    .then(response => response.json())
-    .then(posts => {
-        const postList = document.getElementById('post-list');
-
-        posts.forEach(post => {
-            const postElement = document.createElement('div');
-            postElement.classList.add('post');
-            postElement.innerHTML = `
-                <h2>${post.title.rendered}</h2>
-                <p>${post.content.rendered}</p>
-            `;
-            postList.appendChild(postElement);
-        });
-    })
-    .catch(error => {
-        console.error('Error fetching posts:', error);
-    });
 
 
-    
-const recipeSearchInput = document.getElementById('recipeSearch');
-const recipeSections = document.querySelectorAll('.post');
+document.addEventListener('DOMContentLoaded', () => {
+    const apiUrl = 'https://goodfoodmood0.wordpress.com/wp-json/wp/v2/pages';
 
+    // Function to create page links
+    function createPageLinks(pages) {
+        const menuDropdown = document.getElementById('menu-dropdown');
 
-recipeSearchInput.addEventListener('input', () => {
-  const searchTerm = recipeSearchInput.value.toLowerCase();
-
-  
-  recipeSections.forEach(section => {
-    const recipeTitle = section.querySelector('h2').textContent.toLowerCase();
-    if (recipeTitle.includes(searchTerm)) {
-      section.style.display = 'block';
-    } else {
-      section.style.display = 'none';
+        if (pages.length === 0) {
+            menuDropdown.innerHTML = '<option value="">No pages found</option>';
+        } else {
+            menuDropdown.innerHTML = '<option value="">Select a page</option>';
+            pages.forEach(page => {
+                const option = document.createElement('option');
+                option.value = page.link;
+                option.textContent = page.title.rendered;
+                menuDropdown.appendChild(option);
+            });
+        }
     }
-  });
+
+    // Fetch pages from the WordPress API and populate the dropdown menu
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(pages => {
+            createPageLinks(pages);
+        })
+        .catch(error => {
+            console.error('Error fetching pages:', error);
+        });
+
+    // Handle dropdown menu change
+    menuDropdown.addEventListener('change', (event) => {
+        const selectedPageUrl = event.target.value;
+
+        if (selectedPageUrl) {
+            // If a page is selected, navigate to the URL
+            window.location.href = selectedPageUrl;
+        }
+    });
 });
-
-
 
